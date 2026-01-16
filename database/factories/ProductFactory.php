d307fd12265291c9ed0ade1731c0bbe4fba8661f
Jerald Babor <jeraldbabor@gmail.com>
2026-01-17 03:24:41 +0800

    new fix bug

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -56,7 +56,7 @@ class ProductFactory extends Factory
             'meta_title' => $this->faker->sentence(6),
             'meta_description' => $this->faker->sentence(15),
             'tags' => $this->faker->words(rand(3, 8)),
-            'category_id' => ProductCategory::inRandomOrder()->first()?->id,
+            'category_id' => ProductCategory::factory(),
             'view_count' => $this->faker->numberBetween(0, 1000),
             'order_count' => $this->faker->numberBetween(0, 50),
             'average_rating' => $this->faker->boolean(70) ? $this->faker->randomFloat(1, 3.0, 5.0) : null,
