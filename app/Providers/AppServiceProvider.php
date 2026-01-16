7d1acb02ca64d20c5b871065c375ec82498f2e56
Jerald Babor <jeraldbabor@gmail.com>
2026-01-16 13:47:48 +0800

    production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Listeners\MergeGuestWishlist;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production or when behind a proxy with HTTPS
+        // This fixes mixed content issues where assets are loaded over HTTP
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            // Also force HTTPS if we detect we're behind a proxy with HTTPS
+            URL::forceScheme('https');
+        }
+
         // Merge guest wishlist when user logs in
         Event::listen(
             Login::class,
