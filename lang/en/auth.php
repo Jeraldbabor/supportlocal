dad4dbf686ea2145c44826775d70ffcc4c52ad39
Jerald Babor <jeraldbabor@gmail.com>
2026-02-20 02:42:51 +0800

    asd

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'invalid email and password',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
